97dfc208378ca98b31541a95018c06d40aa5fee3
Andrii Kovalchuk <tieorange@gmail.com>
2017-03-15 23:01:48 +0100

    Finished

--- main_clean.py
+++ main_clean.py
@@ -1,7 +1,9 @@
+from accuracy import get_accuracy
 from dataReader import get_training_set, get_test_set
 
 # data preparation
 from neighbors import get_neighbors
+from response import get_response
 
 trainingSet = get_training_set()
 testSet = get_test_set()
@@ -15,4 +17,10 @@ k = 3
 
 for x in range(len(testSet)):
     neighbors = get_neighbors(trainingSet, testSet[x], k)
+    responses_result = get_response(neighbors)
+    predictions.append(responses_result)
+    print('> predicted = ' + repr(responses_result) + ', actual = ' + repr(testSet[x][-1]))
 
+# accuracy:
+accuracy = get_accuracy(testSet, predictions)
+print('Accuracy: ' + repr(accuracy) + '%')
