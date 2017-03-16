3c8ee3e1129c5fa90cf8b5af62ca0aa08a61781a
Andrii Kovalchuk <tieorange@gmail.com>
2017-03-16 10:24:49 +0100

    User input started

--- main_clean.py
+++ main_clean.py
@@ -1,26 +1,37 @@
 from accuracy import get_accuracy
 from dataReader import get_training_set, get_test_set
-
-# data preparation
 from neighbors import get_neighbors
 from response import get_response
 
-trainingSet = get_training_set()
-testSet = get_test_set()
 
-print trainingSet
-print testSet
+def calculateWithK():
+    k = raw_input("Please input K: ")
+    k = int(k)
+    global neighbors, accuracy
+    # data preparation
+    trainingSet = get_training_set()
+    testSet = get_test_set()
+    print trainingSet
+    print testSet
+    # predictions
+    predictions = []
+    for x in range(len(testSet)):
+        neighbors = get_neighbors(trainingSet, testSet[x], k)
+        responses_result = get_response(neighbors)
+        predictions.append(responses_result)
+        print('> predicted = ' + repr(responses_result) + ', actual = ' + repr(testSet[x][-1]))
 
-# predictions
-predictions = []
-k = 3
+    # accuracy:
+    accuracy = get_accuracy(testSet, predictions)
+    print('Accuracy: ' + repr(accuracy) + '%')
 
-for x in range(len(testSet)):
-    neighbors = get_neighbors(trainingSet, testSet[x], k)
-    responses_result = get_response(neighbors)
-    predictions.append(responses_result)
-    print('> predicted = ' + repr(responses_result) + ', actual = ' + repr(testSet[x][-1]))
 
-# accuracy:
-accuracy = get_accuracy(testSet, predictions)
-print('Accuracy: ' + repr(accuracy) + '%')
+
+# user input
+print('1 - Input K and calculate')
+print('2 - Input your own test data raw')
+print('3 - Draw a graph')
+user_input = raw_input("Please enter something: ")
+
+if user_input == '1':
+    calculateWithK()
